e024560ebe924bc274deff9d95bdf717e306f3d4
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2025-08-16 12:02:09 +0200

    Add weather.py to implement Open-Meteo API client with caching and data processing

---
+++ weather.py
@@ -0,0 +1,53 @@
+import openmeteo_requests
+
+import pandas as pd
+import requests_cache
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+conditions = ["temperature_2m", "cloud_cover", "soil_moisture_3_to_9cm", "soil_temperature_6cm", "showers", "wind_gusts_10m"]
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 52.52,
+	"longitude": 13.41,
+	"hourly": conditions,
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation: {response.Elevation()} m asl")
+print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_cloud_cover = hourly.Variables(1).ValuesAsNumpy()
+hourly_soil_moisture_3_to_9cm = hourly.Variables(2).ValuesAsNumpy()
+hourly_soil_temperature_6cm = hourly.Variables(3).ValuesAsNumpy()
+hourly_showers = hourly.Variables(4).ValuesAsNumpy()
+hourly_wind_gusts_10m = hourly.Variables(5).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["cloud_cover"] = hourly_cloud_cover
+hourly_data["soil_moisture_3_to_9cm"] = hourly_soil_moisture_3_to_9cm
+hourly_data["soil_temperature_6cm"] = hourly_soil_temperature_6cm
+hourly_data["showers"] = hourly_showers
+hourly_data["wind_gusts_10m"] = hourly_wind_gusts_10m
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
